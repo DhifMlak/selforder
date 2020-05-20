b0fafeecd8e05d3a28149eac3fcd113ebef891ce
DhifMlak <Dhifmlak@gmail.com>
2020-05-20 02:01:42 +0200

    add category with menus

--- src/app/pages/categories/add-category/add-category.component.ts
+++ src/app/pages/categories/add-category/add-category.component.ts
@@ -2,6 +2,7 @@ import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { CategoriesService } from 'src/app/providers/categories.service';
 import { MenuService } from 'src/app/providers/menu.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-add-category',
@@ -12,23 +13,32 @@ export class AddCategoryComponent implements OnInit {
   category = { title: '' };
   menus$: any;
   selectable = true;
+  chipControl = new FormControl(new Set());
+
   constructor(private categoryService: CategoriesService, private router: Router,
     private menuService: MenuService) { }
 
   ngOnInit(): void {
     this.menus$ = this.menuService.getMenus();
   }
-  onSubmit() {
-    console.log(this.category);
-    this.categoryService.addCategory(this.category).subscribe(
-      data => {
-        this.router.navigate(['/manager/categories']);
-      }
-    );
+  toggleChip = (chip: any) => {
+    const addChip = () => { this.chips.add(chip); };
+    const removeChip = () => { this.chips.delete(chip); };
 
+    this.chips.has(chip) ? removeChip() : addChip();
+    console.log(this.chips);
   }
-  selected(event) {
-    console.log(event);
+  get chips() {
+    return this.chipControl.value;
+  }
+
+  onSubmit() {
+    const category = { ...this.category, ...{ menus:  Array.from(this.chips) } };
+       this.categoryService.addCategory(category).subscribe(
+        data => {
+          this.router.navigate(['/manager/categories']);
+        }
+      );
   }
 
 
